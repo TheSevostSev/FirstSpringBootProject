d2e5696cf284f61745124c301768f07c3aa467a7
vsevolod <sevostsev@gmail.com>
2024-11-07 19:21:40 +0100

    Changed according to the Jordi's code

--- src/main/java/com/fundesplai/firstspringbootproject/config/SecurityConfig.java
+++ src/main/java/com/fundesplai/firstspringbootproject/config/SecurityConfig.java
@@ -51,14 +51,13 @@ public class SecurityConfig {
 
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable()); // Deshabilitamos la protección contra ataques Cross-site request forgery
+		http.csrf(csrf -> csrf.disable())
+		.cors(withDefaults()); // Deshabilitamos la protección contra ataques Cross-site request forgery
 						// para evitar su configuración en la cabecera de la peticción
 		// Definimos que urls serán públicas
 		http.authorizeHttpRequests((requests) -> {
 			try {
-				requests.requestMatchers("/login").permitAll()
-                        .requestMatchers(HttpMethod.OPTIONS, "/users","/login").permitAll()
-                        .anyRequest().authenticated();
+				requests.requestMatchers("/login").permitAll().anyRequest().authenticated();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
